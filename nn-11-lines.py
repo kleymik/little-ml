55b95f2e82a43c713c309c9d8fbc811cfd3445b4
mikek <michfkl@btinternet.com>
2019-11-12 17:14:20 +0000

    nn-11-lines: add better printing/logging

--- nn-11-lines.py
+++ nn-11-lines.py
@@ -17,163 +17,168 @@ def hyptandh(h): return (1 - h**2)  #  1 - tanh^2(x)
 
 # ReLU
 
-def prtArr(arr, lbl=None):
-    if lbl: print('----', lbl, ' ', arr.shape)
-    for rw in arr:
-        for x in rw: print(f'{x:10.4f}', end='')
-        print()
-    print()
+def prtArr(arr, lbl=None, flatP=False):
+	if lbl: print('----', lbl, ' ', arr.shape)
+	for rw in arr:
+		for x in rw: print(f'{x:10.4f}', end='')
+		if flatP: print(f' - ', end='')
+		else: print()
+	print()
 
 # Data Sets
 
 def getStockSeries(stck='AAPL'):
-    # Import the yfinance. If you get module not found error
-    # the run !pip install yfiannce from your Jupyter notebook
+	# Import the yfinance. If you get module not found error
+	# the run !pip install yfiannce from your Jupyter notebook
 
-    # Get the data of the stock AAPL
-    # data = yf.download('AAPL','2016-01-01','2019-08-01')
-    # Plot the close price of the AAPL
-    # data['Adj Close'].plot()
+	# Get the data of the stock AAPL
+	# data = yf.download('AAPL','2016-01-01','2019-08-01')
+	# Plot the close price of the AAPL
+	# data['Adj Close'].plot()
 
-    # tickers_list = ['AAPL', 'WMT', 'IBM', 'MU', 'BA', 'AXP']
-    tickers_list = ['AAPL', 'IBM']
-    df = pd.DataFrame(columns=tickers_list)
-    # Fetch the data
-    for ticker in tickers_list:
-        df[ticker] = yf.download(ticker,'2019-01-01','2019-08-01')['Adj Close']
-    df = df.resample('W').last()  # Weekly Data
-    df.to_csv('./yfData.csv')
-    print(df.head)
-    return df
+	# tickers_list = ['AAPL', 'WMT', 'IBM', 'MU', 'BA', 'AXP']
+	tickers_list = ['AAPL', 'IBM']
+	df = pd.DataFrame(columns=tickers_list)
+	# Fetch the data
+	for ticker in tickers_list:
+		df[ticker] = yf.download(ticker,'2019-01-01','2019-08-01')['Adj Close']
+	df = df.resample('W').last()  # Weekly Data
+	df.to_csv('./yfData.csv')
+	print(df.head)
+	return df
 
 def getCheckDummyData0():
-    IN = np.array([[ 0.0, 0.0, 1.0],       # 4 X lev0Num
-                   [ 1.0, 1.0, 1.0],
-                   [ 1.0, 0.0, 1.0],
-                   [ 0.0, 1.0, 1.0]])
-    OUT = np.array([[0.0],                 # 4 x lev2Num
-                    [1.0],
-                    [1.0],
-                    [0.0]])
-    return IN, OUT
+	IN = np.array([[ 0.0, 0.0, 1.0],	   # 4 X lev0Num
+				   [ 1.0, 1.0, 1.0],
+				   [ 1.0, 0.0, 1.0],
+				   [ 0.0, 1.0, 1.0]])
+	OUT = np.array([[0.0],				 # 4 x lev2Num
+					[1.0],
+					[1.0],
+					[0.0]])
+	return IN, OUT
 
-def getCheckDummyData():
-    IN = np.array([[ 0.0, 0.5, 1.0, 1.0],   # 5 X lev0Num
-                   [ 1.0, 1.0, 1.0, 1.0],
-                   [ 1.0, 0.0, 1.0, 0.0],
-                   [ 1.0, 0.8, 1.0, 0.2],
-                   [ 0.0, 1.0, 1.0, 0.0]])
-    OUT = np.array([[0.5, 1.0],            # 5 x lev2Num
-                    [1.0, 0.0],
-                    [1.0, 0.3],
-                    [0.6, 0.8],
-                    [0.9, 0.0]])
-    return IN, OUT
+def getCheckDummyData5():
+	IN = np.array([[ 0.0, 0.5, 1.0, 1.0],   # 5 X lev0Num
+				   [ 1.0, 1.0, 1.0, 1.0],
+				   [ 1.0, 0.0, 1.0, 0.0],
+				   [ 1.0, 0.8, 1.0, 0.2],
+				   [ 0.0, 1.0, 1.0, 0.0]])
+	OUT = np.array([[0.5, 1.0],			# 5 x lev2Num
+					[1.0, 0.0],
+					[1.0, 0.3],
+					[0.6, 0.8],
+					[0.9, 0.0]])
+	return IN, OUT
 
 
-def train(IN=None, OUT=None, midLayer=3):  # number of middle-level neurons
-    lev0Num = IN.shape[1]          # 4 cols=number of indep inputs
-    lev1Num = midLayer             # 4 middle layer
-    lev2Num = OUT.shape[1]         # 2 cols=number of indep inputs
+def train(IN=None, OUT=None, midLayer=3, iterLim=1e6, errThresh=1e-4):  # number of middle-level neurons
+	lev0Num = IN.shape[1]		 # 4 cols=number of indep inputs
+	lev1Num = midLayer			 # 4 middle layer
+	lev2Num = OUT.shape[1]		 # 2 cols=number of indep inputs
 
-    seed(1)
-    syn01 = 2 * random((lev0Num, lev1Num)) - 1
-    syn12 = 2 * random((lev1Num, lev2Num)) - 1
+	seed(1)
+	syn01 = 2 * random((lev0Num, lev1Num)) - 1
+	syn12 = 2 * random((lev1Num, lev2Num)) - 1
 
-    print('------------- Start')
-    prtArr(IN,    'IN')
-    prtArr(syn01, 'syn01')
-    prtArr(syn12, 'syn12')
-    prtArr(OUT,   'OUT')
+	print('------------- Start')
+	prtArr(IN,	'IN = numTrials X numInputs')
+	prtArr(syn01, 'syn01 = numInputs x numMidLayer')
+	prtArr(syn12, 'syn12 = numTrials x numMidLayer')
+	prtArr(OUT,   'OUT = numTrials x numOuputs')
 
-    for j in range(int(1e5)):
+	for j in range(int(iterLim)):
 
-        lev1 = sigmoid(  IN @ syn01)    # fwd prop the response
-        lev2 = sigmoid(lev1 @ syn12)
-                                        # pdb.set_trace()
-        outErrs = (OUT - lev2)          # back prop the error
-        lev2Dlta = outErrs            * sigmoidds(lev2, accel=1.0)
-        lev1Dlta = lev2Dlta @ syn12.T * sigmoidds(lev1, accel=1.0)
+		lev1 = sigmoid(  IN @ syn01)  # fwd prop the response
+		lev2 = sigmoid(lev1 @ syn12)
+									  # pdb.set_trace()
+		outErrs = (OUT - lev2)		  # back prop the error
+		lev2Dlta = outErrs			  * sigmoidds(lev2, accel=1.0)
+		lev1Dlta = lev2Dlta @ syn12.T * sigmoidds(lev1, accel=1.0)
 
-        syn12 += lev1.T @ lev2Dlta      # update synapse weights
-        syn01 += IN.T   @ lev1Dlta
-        if (j % 1000==0):
-            print(f'{j} ', end='')
-            print('err=', (outErrs * outErrs).sum())
-        if (outErrs * outErrs).sum() < 1e-5: # sum of sqrd errors
-            print(outErrs)
-            print()
-            break
+		syn12 += lev1.T @ lev2Dlta	  # update synapse weights
+		syn01 += IN.T   @ lev1Dlta
+		if (j % 10000==0):
+			print(f'{j} ', end='')
+			print('err=', (outErrs * outErrs).sum(), end='')
+			printArr(syn01, flatP=True)
+			print()
+		if (outErrs * outErrs).sum() < errThresh: # sum of sqrd errors
+			print(outErrs)
+			print()
+			break
 
-    print('------------- Done')
-    prtArr(IN,    'IN')
-    prtArr(syn01, 'syn01')
-    prtArr(lev1,  'lev1')
-    prtArr(syn12, 'syn12')
-    prtArr(lev2, 'lev2')
-    prtArr(OUT,   'OUT')
-    print('err=',(outErrs * outErrs).sum())
+	print('------------- Done')
+
+	prtArr(IN,	  'IN    = numTrials X numInputs')
+	prtArr(syn01, 'syn01 = numInputs x numMidLayer')
+	prtArr(lev1,  'lev1  = numTrials x numMidLayer')
+	prtArr(syn12, 'syn12 = numTrials x numMidLayer')
+	prtArr(lev2,  'lev2  = numTrials x numOutputs' )
+	prtArr(OUT,   'OUT   = numTrials x numOuputs')
+
+	print('err=',(outErrs * outErrs).sum())
 
 
 # if True: df = getStockSeries(stck='AAPL')  # as weekly
-# else:    df = pd.read_csv('./yfData.csv')
+# else:	df = pd.read_csv('./yfData.csv')
 # print(df.head)
 # print(df.shape)
 # # 1=dummy variable to identify AAPL
 # # inSet = [ np.append(df['AAPL'].loc[-di:-di+6].values, 1) for di in range(-30,-20) ] # 10 trainig sample of six consectutive weeks
-# numTrain     =  15 # number of training samples
+# numTrain	 =  15 # number of training samples
 # tsPttrnLen   =   4 # length of input pattern timeseries
 # seriesStrt   = -25
 # scaleFactor  = 1e-3
 # inSet = [ np.append(df[di:di+tsPttrnLen].AAPL.values * scaleFactor, 1)
-#           for di in range(seriesStrt, seriesStrt + numTrain) ] # 10 training sample of six consecutive wkly rtns
+#		   for di in range(seriesStrt, seriesStrt + numTrain) ] # 10 training sample of six consecutive wkly rtns
 # # TBD convert into Buy Sell: increase => Sell, decrease=>Buy
 # otSet = [ np.append(df[di+tsPttrnLen:di+tsPttrnLen+2].AAPL.values * scaleFactor, 1)
-#           for di in range(seriesStrt,seriesStrt + numTrain) ] # try to predict next 2 wkly rtns
+#		   for di in range(seriesStrt,seriesStrt + numTrain) ] # try to predict next 2 wkly rtns
 
 def main():
 
-    numTrain     =  15 # number of training samples
-    tsPttrnLen   =   4 # length of input pattern timeseries
-    seriesStrt   = -25
-    scaleFactor  = 1e-3
+	numTrain	 =  15 # number of training samples
+	tsPttrnLen   =   4 # length of input pattern timeseries
+	seriesStrt   = -25
+	scaleFactor  = 1e-3
 
-    if True:
-        IN, OUT = getCheckDummyData0()
-        train(IN=IN, OUT=OUT)
-    if False:
-        if True: df = getStockSeries(stck='AAPL')  # as weekly
-        else:    df = pd.read_csv('./yfData.csv')
-        print(df.head)
-        print(df.shape)
-        # 1=dummy variable to identify AAPL
-        # inSet = [ np.append(df['AAPL'].loc[-di:-di+6].values, 1)
-        #    for di in range(-30,-20) ] # 10 trainig sample of 6 consecve weeks
-        inSet = [ np.append(df[di:di+tsPttrnLen].AAPL.values * scaleFactor, 1)
-                  for di in range(seriesStrt, seriesStrt + numTrain) ]
-        # 10 training sample of six consecutive wkly rtns
-        # TBD convert into Buy Sell: increase => Sell, decrease=>Buy
-        otSet = [ np.append(df[di+tsPttrnLen:di+tsPttrnLen+2].AAPL.values *
-                            scaleFactor, 1)
-                  for di in range(seriesStrt,seriesStrt + numTrain) ]
-        # try to predict next 2 wkly rtns
-        
-        train(IN=np.vstack(inSet), OUT=np.vstack(otSet), lev1Num=10)
-        #train(IN=df.values, OUT=dfb.values, lev1Num=4)
-        #for v in res['Adj Close'][-100:]: print(v)
+	if True:
+		IN, OUT = getCheckDummyData5()
+		train(IN=IN, OUT=OUT)
+	if False:
+		if True: df = getStockSeries(stck='AAPL')  # as weekly
+		else:	df = pd.read_csv('./yfData.csv')
+		print(df.head)
+		print(df.shape)
+		# 1=dummy variable to identify AAPL
+		# inSet = [ np.append(df['AAPL'].loc[-di:-di+6].values, 1)
+		#	for di in range(-30,-20) ] # 10 trainig sample of 6 consecve weeks
+		inSet = [ np.append(df[di:di+tsPttrnLen].AAPL.values * scaleFactor, 1)
+				  for di in range(seriesStrt, seriesStrt + numTrain) ]
+		# 10 training sample of six consecutive wkly rtns
+		# TBD convert into Buy Sell: increase => Sell, decrease=>Buy
+		otSet = [ np.append(df[di+tsPttrnLen:di+tsPttrnLen+2].AAPL.values *
+							scaleFactor, 1)
+				  for di in range(seriesStrt,seriesStrt + numTrain) ]
+		# try to predict next 2 wkly rtns
+		
+		train(IN=np.vstack(inSet), OUT=np.vstack(otSet), lev1Num=10)
+		#train(IN=df.values, OUT=dfb.values, lev1Num=4)
+		#for v in res['Adj Close'][-100:]: print(v)
 
 main()
 
 # junk
-    # if IN is None:
-    #     IN = np.array([[ 0.0, 0.5, 1.0, 1.0],   # 5 X lev0Num
-    #                    [ 1.0, 1.0, 1.0, 1.0],
-    #                    [ 1.0, 0.0, 1.0, 0.0],
-    #                    [ 1.0, 0.8, 1.0, 0.2],
-    #                    [ 0.0, 1.0, 1.0, 0.0]])
-    # if OUT is None:
-    #     OUT = np.array([[0.5, 1.0],            # 5 x lev2Num
-    #                     [1.0, 0.0],
-    #                     [1.0, 0.3],
-    #                     [0.6, 0.8],
-    #                     [0.9, 0.0]])
+	# if IN is None:
+	#	 IN = np.array([[ 0.0, 0.5, 1.0, 1.0],   # 5 X lev0Num
+	#					[ 1.0, 1.0, 1.0, 1.0],
+	#					[ 1.0, 0.0, 1.0, 0.0],
+	#					[ 1.0, 0.8, 1.0, 0.2],
+	#					[ 0.0, 1.0, 1.0, 0.0]])
+	# if OUT is None:
+	#	 OUT = np.array([[0.5, 1.0],			# 5 x lev2Num
+	#					 [1.0, 0.0],
+	#					 [1.0, 0.3],
+	#					 [0.6, 0.8],
+	#					 [0.9, 0.0]])
