9ef688812697aeaac7dcd1447169097b09274c43
Mike Kleyn <michfkl@btinternet.com>
2020-06-10 22:14:19 +0100

    ml: name synapse by the layers they connect; print array with label

--- backProp.py
+++ backProp.py
@@ -6,13 +6,13 @@ from numpy.random import random, seed
 def sigmoid(x):  return 1 / (1+np.exp(-x))
 def sigmoidd(x): return sigmoid(x) * (1 - sigmoid(x)) # derivative of sigmoid in terms of sigmoid
 def sigmoidds(s): return s * (1 - s)
-def prtArr(arr):
+def prtArr(arr, lbl=None):
+    if lbl: print('----',lbl)
     for rw in arr:
-        for x in rw:
-            print(f'{x:4.3f}  ', end='')
+        for x in rw: print(f'{x:4.3f}  ', end='')
         print()
-        # print(f'{x[0]:4.3f}  ', end='')
         # print(sum(abs(lev2Dlta)))
+    print()
 
 def go():
 
@@ -30,24 +30,31 @@ def go():
                        1,
                      0.75,
                      0.5]]).T
-    syn0 = 2*np.random.random((inSize, midSize)) - 1 # 12 +/- 1 adjustable loadings on links gives 3x4 cross-wire to hidden layer
-    syn1 = 2*np.random.random((midSize,      1)) - 1 #  4 +/- 1 adjsutable loadings onto one output node
+
+    syn01 = 2*np.random.random((inSize, midSize)) - 1 # 12 +/- 1 adjustable loadings on links gives 3x4 cross-wire to hidden layer
+    syn12 = 2*np.random.random((midSize,      1)) - 1 #  4 +/- 1 adjsutable loadings onto one output node
 
     lev0 = IN                               # 4 samples of input triple
+
+    print('------------- Start')
+    prtArr(IN,    'IN')
+    prtArr(syn01, 'syn01')
+    prtArr(syn12, 'syn12')
+    prtArr(OUT,   'OUT')
 
     for j in range(int(1e5)):
 
         # forward prop the response
-        lev1 = sigmoid(lev0 @ syn0)         # l1 = level 1 neuron state
-        lev2 = sigmoid(lev1 @ syn1)
+        lev1 = sigmoid(lev0 @ syn01)         # l1 = level 1 neuron state
+        lev2 = sigmoid(lev1 @ syn12)
 
         # backprop the error
         outErrs = (OUT - lev2)
         lev2Delta = outErrs * sigmoidds(lev2)
         syn1 += lev1.T @ lev2Delta
 
-        lev1Delta = lev2Delta @ syn1.T * sigmoidds(lev1)
-        syn0 += lev0.T @ lev1Delta
+        lev1Delta = lev2Delta @ syn12.T * sigmoidds(lev1)
+        syn01 += lev0.T @ lev1Delta
 
         if j % 100 == 0: print('SQERR=', j, (ssq:= outErrs.T @ outErrs)[0][0])
         if (ssq) < 1e-4: # sum of squared errors
