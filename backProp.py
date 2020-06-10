f95ef086a4dc3e9b113083cc4e85d89d49d7b45c
Mike Kleyn <michfkl@btinternet.com>
2020-06-10 21:11:20 +0100

    ml: add sigmoid deriv, deterministic random via seed, parameterised layer sizes,

--- backProp.py
+++ backProp.py
@@ -2,30 +2,35 @@
 
 import numpy as np
 
-def sigmoid(x): return 1 / (1+np.exp(-x))
+def sigmoid(x):  return 1 / (1+np.exp(-x))
+def sigmoidd(x): return sigmoid(x) * (1 - sigmoid(x)) # derivative of sigmoid in terms of sigmoid
 
 def go():
-    IN = np.array([[0, 0, 1],               # 4 samples of input triple
-                   [1, 1, 1],
-                   [0, 1, 1],
-                   [1, 0, 1] ])
+
+    inSize  = 4  # input dimensionality
+    midSize = 4  # m 1 middle/ hidden layer
+    outSize = 1  # output dimensionality
+    np.random.seed(1)
+
+    IN = np.array([[0, 0, 1, 1],            # 4 samples of input triple
+                   [1, 1, 1, 1],
+                   [0, 1, 1, 1],
+                   [1, 0, 1, 0]])
+
     OUT = np.array([[0.5,                   # desired output for each sample
                        1,
-                     0.8,
-                     0.7]]).T
-
-    syn0 = 2*np.random.random((3,4)) - 1    # 12 +/- 1 adjustable loadings on links gives 3x4 cross-wire to hidden layer
-    syn1 = 2*np.random.random((4,1)) - 1    #  4 +/- 1 adjsutable loadings onto one output node
+                     0.75,
+                     0.5]]).T
+    syn0 = 2*np.random.random((inSize, midSize)) - 1 # 12 +/- 1 adjustable loadings on links gives 3x4 cross-wire to hidden layer
+    syn1 = 2*np.random.random((midSize,      1)) - 1 #  4 +/- 1 adjsutable loadings onto one output node
 
     lev0 = IN                               # 4 samples of input triple
-
-
 
     for j in range(int(1e5)):
 
         # forward prop the response
-        lev1 = 1 / (1+np.exp(-(lev0 @ syn0)))  # l1 = level 1 neuron state
-        lev2 = 1 / (1+np.exp(-(lev1 @ syn1)))
+        lev1 = sigmoid(lev0 @ syn0)         # l1 = level 1 neuron state
+        lev2 = sigmoid(lev1 @ syn1)
 
         # backprop the error
         outErrs = (OUT - lev2)
@@ -34,11 +39,14 @@ def go():
 
         lev1_delta = lev2_delta @ syn1.T * (lev1 * (1 - lev1))
         syn0 += lev0.T @ lev1_delta
-        if j % 1000 == 0: print('SQERR=',j, (outErrs * outErrs).sum())
-        if (outErrs * outErrs).sum() < 1e-8: # sum of squard errors
+
+        if j % 100 == 0: print('SQERR=', j, (ssq:= outErrs.T @ outErrs)[0][0])
+        if (ssq) < 1e-4: # sum of squared errors
             print(outErrs.T)
             print()
             break
 
     print('RES=', lev2)
-    print('SQERR=', outErrs.T @ outErrs)
+    print('SQERR=', ssq)
+
+if __name__ == '__main__': go()
